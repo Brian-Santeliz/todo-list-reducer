8e39eae203f9c139f0d017ed0d64738dcad94f55
Brian <blsvsanteliz@gmail.com>
2020-11-07 11:01:16 -0400

    Test deploy

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
   return (
     <nav className="navbar navbar-dark bg-primary d-flex justify-content-center">
       <h3 className="text-white mt-2">
-        Todo's App ({state.filter((todo) => !todo.complete).length} task to do)
+        Todo's App! ({state.filter((todo) => !todo.complete).length} task to do)
       </h3>
     </nav>
   );
