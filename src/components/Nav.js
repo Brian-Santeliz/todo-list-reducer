b4f08c396787f5a929be91d49d795e93e949d2c1
Brian <blsvsanteliz@gmail.com>
2020-11-09 14:14:31 -0400

    Center navbar text

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
   const { state } = useContext(Context);
   return (
     <Navbar className="bg-primary p-2 d-flex justify-content-center">
-      <h3 className="text-white mt-2">
+      <h3 className="text-white mt-2 text-center">
         Todo's App! ({state.filter((todo) => !todo.complete).length} task to do)
       </h3>
     </Navbar>
